e250886d20a9def29fcd5befbd951fae85015e4e
thangnd46 <thangnd46@viettel.com.vn>
2021-01-08 08:51:13 +0700

    udpate

--- app/src/main/java/com/example/samsungtvcontrol/MainActivity.java
+++ app/src/main/java/com/example/samsungtvcontrol/MainActivity.java
@@ -29,11 +29,20 @@ import com.example.samsungtvcontrol.tvcommand.SearchListener;
 import com.samsung.multiscreen.Search;
 import com.samsung.multiscreen.Service;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private static Search search;
@@ -289,16 +298,18 @@ public class MainActivity extends AppCompatActivity {
 //                    samsungWebsocket.sendKey(Keycode.KEY_ENTER.toString(), 1, "Click");
                     YoutubeService.openVideo(samsungWebsocket, text);
 //                    GoogleService.searchAndOpen(samsungWebsocket, "Di ve nha");
-//                    OkHttpClient client = new OkHttpClient();
-//                    Request request = new Request.Builder()
-//                            .url("localhost:5000/execute-text").addHeader("Text", text)
-//                            .build();
-//                    try {
-//                        Response response = client.newCall(request).execute();
-//                        //todo viet them xu ly o day
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url("localhost:5000/execute-text").addHeader("Text", text)
+                            .build();
+                    try {
+                        Response response = client.newCall(request).execute();
+                        String responseString = Objects.requireNonNull(response.body()).string();
+                        JSONObject jsonObject=new JSONObject(responseString);
+                        //todo viet them xu ly o day
+                    } catch (IOException | JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             }
